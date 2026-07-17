53bf58b6580c21a11457a07c94ec868086d1e5f2
Brahamjeet Anand <brahamjeetrockzz@gmail.com>
2026-07-18 02:13:00 +0530

    Initial commit

---
+++ closures and lexical scoping.js
@@ -0,0 +1,12 @@
+function init() {
+  var a = 5;
+
+  function inc() {
+    ++a;
+    console.log(a);
+  }
+
+  inc();
+}
+
+init();
---
+++ index.js
@@ -0,0 +1,36 @@
+// // let a=25;
+
+// // console.log(a);
+
+// function add() {
+//   const name = "Brahamjeet";
+
+//   function sub() {
+//     const age = "twenty five";
+
+//     console.log(age);
+//   }
+
+//   // console.log(age);
+
+//   console.log(age);
+
+// sub();
+
+
+// }
+
+// add();
+
+console.log(addon(6));
+
+function addon(num){
+
+  return num+2;
+}
+
+
+console.log(a(10));
+const a=function(num){
+  return num+5;
+}
