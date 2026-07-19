c3c5e28f37694ecbe35b87d95aa9646dd253bbfb
Brahamjeet Anand <brahamjeetrockzz@gmail.com>
2026-07-19 22:33:51 +0530

    new commit

---
+++ callback based.js
@@ -0,0 +1,12 @@
+
+setTimeout(()=>{
+  console.log("This Executes first call back");
+},200)
+
+setTimeout(()=>{
+  console.log("This Executes Later call back")
+},400)
+
+
+console.log("I have been executed first ");
+
--- promises.js
+++ promises.js
@@ -1,17 +1,62 @@
-let res = 5 + 3;
+// let res = 5 + 3;
 
+// async function data() {
+//   let res2 = await fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur");
 
-async function data() {
-  let res2 = await fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur");
+//   console.log( res2);
+// }
 
-  console.log( res2);
-}
+// data();
 
-
-data();
-
-
-console.log(res);
+// console.log(res);
 
 //building my own  promises
+
+let ans = new Promise((res, rej) => {
+  console.log("Hi Brother");
+});
+
+// ans();
+
+ans;
+
+//SetTimeout
+
+let ans2 = new Promise(() => {
+
+  function hello() {
+    console.log("Hello my name is gyan");
+  }
+ 
+
+   
+ setTimeout(hello,1)
+})
+
+// undertanding then and catch
+
+let ans3=new Promise((resolve,reject)=>{
+
+  let a=2
+
+  if(a>=2)
+  {
+    resolve("Its a positive number")    //end up in fullfilled state as it satisfies condition of resolve
+  }
+  else if(a<0)
+  {
+    reject("Its a negative number")
+  }
+
+})
+
+ans3.then((message)=>{
+  console.log("The status of promise is currently: "+message)
+
+  return "positive number is 2"
+}).catch((error)=>{
+   console.log("The error message of the program is :"+ error);
+}).then((message)=>{
+  console.log("Second message is: "+message);
+})
 
